bc9bcface2c47b64af44ffcb9df19dff50261609
A01Akshat <104577005+A01Akshat@users.noreply.github.com>
2023-01-19 23:17:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import REACT from "react";
+import  ReactDOM from "react-dom";
+import App3 from "./App3";
+import ComA from "./ComA";
+
+
+
+ReactDOM.render(<ComA />,document.getElementById('root'));
+ 
